0e91bdf094abc6bedea81ad4513311194388baf3
Kalid Diriye <45129382+KD77@users.noreply.github.com>
2021-07-06 11:55:27 +0300

    Added firebase functions and mail methods

---
+++ functions/index.js
@@ -0,0 +1,45 @@
+const functions = require('firebase-functions');
+const nodemailer = require('nodemailer')
+const cors = require('cors')({
+  origin: true
+})
+const gmailEmail= functions.config().gmail.email
+const gmailPassword= functions.config().gmail.password
+
+const mailTransport= nodemailer.createTransport({
+  service: 'gmail',
+  auth:{
+    user:gmailEmail,
+    pass:gmailPassword,
+  },
+})
+exports.submit= functions.https.onRequest((req, res)=>{
+  res.set('Access-Control-Allow-Origin', '*')
+  res.set('Access-Control-Allow-Methods', 'GET, PUT, POST, OPTIONS')
+  res.set('Access-Control-Allow-Headers', '*')
+  if( req.method=== 'OPTION'){
+    res.end
+  }else{
+    cors(req, res,()=>{
+      if (req.method!== 'POST'){
+        return
+      }
+      const mailOptions = {
+        from: req.body.email,
+        replyTo: req.body.email,
+        to: gmailEmail,
+        subject: `${req.body.name}messaged you from your webite` ,
+        text: req.body.message,
+        html:`<p>${req.body.message}</p>`,
+
+      }
+      return mailTransport.sendMail(mailOptions).then(()=>{
+        console.log('New email sent to:',gmailEmail)
+        res.status(200).send({
+          isEmailSent: true
+        })
+        return
+      })
+    })
+  }
+})
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,10 +1,45 @@
 import React, { Component } from 'react'
-
+import {firestore} from '../../Database/firebase.utils'
+import './contact.css'
 export default class Contact extends Component {
+  constructor(props) {
+    super()
+    this.state = {
+      name:'',
+      email:'',
+      message:'',
+    }
+  }
   render() {
     return (
-      <div>
-      <h1>hello from contact</h1>
+      <div className="container">
+     <form id="contact">
+       <label>
+         <input
+         type="text"
+         name="name"
+         placeholder="Your Name"
+         />
+       </label>
+       <label>
+         <input
+         type="text"
+         name="email"
+         placeholder="Enter email"
+         />
+         </label>
+         <label>
+           <textarea
+           placeholder="message..."
+           require
+           ></textarea>
+         </label>
+         <div>
+           <button type="submit">submit</button>
+         </div>
+
+      
+     </form>
       </div>
     )
   }
